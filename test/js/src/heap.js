e210c3fb1e8f3c3430661b29cccae56b0a684d03
aureooms <aurelien.ooms@gmail.com>
2015-09-17 10:59:31 +0000

    complete coverage for heap

--- test/js/src/heap.js
+++ test/js/src/heap.js
@@ -1,4 +1,3 @@
-
 var measure = require( 'aureooms-js-measure' ) ;
 var fingertree = require( 'aureooms-js-fingertree' ) ;
 
@@ -17,6 +16,25 @@ test( 'heap' , function ( assert ) {
 	var b = y[0] ;
 	var z = y[1].extractMax( ) ;
 	var c = z[0] ;
+
+	assert.equal( a , 3 ) ;
+	assert.equal( b , 2 ) ;
+	assert.equal( c , 1 ) ;
+
+	assert.ok( z[1].empty( ) ) ;
+	assert.equal( h.maxKey( ) , 3 ) ;
+	assert.equal( x[1].maxKey( ) , 2 ) ;
+	assert.equal( y[1].maxKey( ) , 1 ) ;
+	assert.equal( z[1].maxKey( ) , -Infinity ) ;
+
+	h = Heap.from( [ 1 , 3 , 2 ] ) ;
+
+	 x = h.extractMax( ) ;
+	 a = x[0] ;
+	 y = x[1].extractMax( ) ;
+	 b = y[0] ;
+	 z = y[1].extractMax( ) ;
+	 c = z[0] ;
 
 	assert.equal( a , 3 ) ;
 	assert.equal( b , 2 ) ;
