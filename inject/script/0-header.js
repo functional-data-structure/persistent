eaa9fc353a0029fadbc42ca97082dac524b2600a
aureooms <aureooms@users.noreply.github.com>
2021-03-25 17:04:05 +0000

    Deploying to gh-pages from @ aureooms/js-persistent@6ab7775594e60c6929d47704016f4e8452c43e9b 🚀

---
+++ inject/script/0-header.js
@@ -0,0 +1,30 @@
+const domReady = function (callback) {
+	const state = document.readyState;
+	if (state === 'interactive' || state === 'complete') {
+		callback();
+	} else {
+		document.addEventListener('DOMContentLoaded', callback);
+	}
+};
+
+domReady(() => {
+	const projectname = document.createElement('a');
+	projectname.classList.add('project-name');
+	projectname.text = 'aureooms/js-persistent';
+	projectname.href = './index.html';
+
+	const header = document.querySelector('header');
+	header.insertBefore(projectname, header.firstChild);
+
+	const testlink = document.querySelector('header > a[data-ice="testLink"]');
+	testlink.href = 'https://coveralls.io/github/aureooms/js-persistent';
+	testlink.target = '_BLANK';
+
+	const searchBox = document.querySelector('.search-box');
+	const input = document.querySelector('.search-input');
+
+	// Active search box when focus on searchBox.
+	input.addEventListener('focus', () => {
+		searchBox.classList.add('active');
+	});
+});
